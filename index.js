35e07b0a641a8234c38e84fb98813c2fe176bff6
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2022-11-01 17:02:23 +0800

    Fix 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ObjectId } = require ("mongodb")
 const express = require("express")
+const favicon = require('express-favicon')
 const multer = require("multer")
 const path = require("path")
 const sanitizeHTML = require("sanitize-html")
@@ -54,6 +55,7 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use(favicon(__dirname + '/public/favicon.png'))
 
 //Sochet.IO---------------------------------------------------------------------------------------------
 let users = []
