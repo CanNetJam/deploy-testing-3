97430b13148e1d62d9e6450c8850799945f1a3f4
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2022-11-03 11:06:57 +0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -46,7 +46,7 @@ function App() {
   
   //ws://deploy-testing-3.herokuapp.com/socket.io/?EIO=4&transport=websocket
   useEffect(()=> {
-    setSocket(io("https://deploy-testing-3.herokuapp.com/"))
+    setSocket(io("ws://deploy-testing-3.herokuapp.com/"))
   }, [])
 
   useEffect(() => {
