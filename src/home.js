6ab9339171f8464f8742c54db724d4fc38c7bb2a
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2023-01-11 17:42:07 +0800

    debug test 20

--- src/home.js
+++ src/home.js
@@ -41,7 +41,7 @@ function App() {
   const [ number, setNumber ] = useState(0)
 
   useEffect(()=> {
-    setSocket(io("ws://deploy-testing-3.onrender.com:8080"))
+    setSocket(io("ws://deploy-testing-3.onrender.com"))
   }, [])
 
   useEffect(() => {
