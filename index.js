32e2258d5ba16ad52963b39539697da92f155112
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2023-01-12 20:19:44 +0800

    debug test 29

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(favicon(__dirname + '/public/favicon.png'))
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, 
   console.log(`Node server started at port ${PORT}`)
