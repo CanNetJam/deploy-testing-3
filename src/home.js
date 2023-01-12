e669d74bdf3f3ac7f925ab6de50d3f490265f60b
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2023-01-12 20:38:24 +0800

    debug test 31

--- src/home.js
+++ src/home.js
@@ -5,9 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { io } from "socket.io-client"
 
 import TopNav from "./navigation/TopNav"
-import LeftNavFree from "./navigation/LeftNavFree"
-import LeftNavEmp from "./navigation/LeftNavEmp"
-import LeftNavAdmin from "./navigation/LeftNavAdmin"
 
 import Home from "./pages/Homes"
 import About from "./pages/About"
@@ -46,7 +43,7 @@ function App() {
   //http://localhost:3000
   //https://deploy-testing-3.onrender.com
   useEffect(()=> {
-    setSocket(io("https://deploy-testing-3.onrender.com:10000"))
+    setSocket(io("https://deploy-testing-3.onrender.com"))
   }, [])
 
   useEffect(() => {
