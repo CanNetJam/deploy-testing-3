b9dc70cc0e7d5b235ac05232881160f83bf4c2c2
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2023-03-10 20:14:48 +0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -44,7 +44,10 @@ function App() {
   //http://localhost:3000
   //https://deploy-testing-3.onrender.com
   useEffect(()=> {
-    setSocket(io("https://deploy-testing-3.onrender.com"))
+    setSocket(io("https://deploy-testing-3.onrender.com", 
+    {
+      "transports": ['websocket']
+    }))
   }, [])
 
   useEffect(() => {
