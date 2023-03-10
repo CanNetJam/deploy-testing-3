bd58e079fb97309837ca7840c1035158862b77d3
Kenneth John V. Saracho <kjvs.aw@gmail.com>
2023-03-10 18:58:19 +0800

    Update Conversation.js

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from "react"
 import Axios from "axios"
 
+
 function Conversation({conversation, currentUser}) {
+    const cloud_name = "dzjkgjjut"
     const [user, setUser] = useState(null)
     const [pic, setPic] = useState()
     
@@ -11,7 +13,7 @@ function Conversation({conversation, currentUser}) {
           try {
             const res = await Axios.get(`/members/${memberId}`)
             setUser(res.data[0].firstname)//need to find the firstname of the userId 
-            setPic(res.data[0].photo)
+            setPic(res.data[0].image)
           } catch (err) {
             console.log(err)
           }
@@ -21,7 +23,7 @@ function Conversation({conversation, currentUser}) {
     
     return (
         <div className="conversation">
-            <img className="conversationImg" src={pic ? `/uploaded-photos/${pic}` : "/fallback.png"} alt=""/>
+            <img src={pic ? `https://res.cloudinary.com/${cloud_name}/image/upload/w_300,h_200,c_fill,q_85/${pic}.jpg` : "/fallback.png"} className="messageImg"></img>
             <span className="conversationName">{user}</span>
         </div>
     )
